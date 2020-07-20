574a5d0bb257e20ef9afd343410af16f74224b9e
Dalanah12 <Dalanah12@gmail.com>
2020-07-20 10:02:35 -0400

    Updated apiConfig.js with deployed heroku url updated package.json with github pages url
    Co-authored-by: Andrew i.am.andrewchun@gmail.com
    Co-authored-by: Dalanah dalanah12@gmail.com
    Co-authored-by: Frederick frederickwoodhouse@gmail.com
    Co-authored-by: Kyle kbini28@gmail.com

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://whispering-ridge-76247.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
